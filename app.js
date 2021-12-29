c9004005157cf6fc2bb59e5ead178a6986b7e9d9
stepan323446 <66179087+stepan323446@users.noreply.github.com>
2021-12-29 23:25:04 +0100

    .

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
 let friends = new Array();
 let favourites = new Array();
-let settingsData;
+let settingsData = new Settings();
+
+let versionPlugin = "1.1.0";
+let linkToAuthor = "https://www.youtube.com/channel/UC-X7_-qML3aXqrDQ2UKLFkA";
 
 function Friend(userEl) {
     this.element = userEl;
@@ -42,13 +45,29 @@ function Friend(userEl) {
 }
 function Settings()
 {
-    this.darkTheme = true;
+    this.darkTheme = false;
+    this.squareImage = false;
 
-    this.loadData = function()
+    this.setData = function()
     {
-        chrome.storage.local.set({ "MPPL-settings-data": settings }, function () {
+        chrome.storage.local.set({ "MPPL-settings-data": settingsData }, function () {
             console.log("MPPL Loaded data for settings (Added)");
         });
+    }
+    this.applySettings = function()
+    {
+        let body = document.body;
+        if(this.darkTheme)
+            body.classList.add("MPPL-S-DarkTheme");
+        else
+            body.classList.remove("MPPL-S-DarkTheme");
+
+        if(this.squareImage)
+            body.classList.add("MPPL-S-square-portrait");
+        else
+            body.classList.remove("MPPL-S-square-portrait");
+        console.log(settingsData);
+        console.log("Settings applied");
     }
 }
 
@@ -62,33 +81,108 @@ catch
     console.log("Not found data");
 }
 try {
+    console.log("Checking....");
     chrome.storage.local.get(["MPPL-settings-data"], function (items) {
-        settingsData = items["MPPL-settings-data"];
-    });
+        settingsData.darkTheme = items["MPPL-settings-data"].darkTheme;
+        settingsData.squareImage = items["MPPL-settings-data"].squareImage;
 
-    if(settingsData == "")
-    {
-        settingsData = standartSetting();
-    }
+        settingsData.applySettings();
+    });
 }
 catch
 {
     console.log("Not found data");
+    standartSetting();
 }
 
 function standartSetting()
 {
-    let standart = new Settings();
-    standart.darkTheme = false;
-    
-    return standartSetting();
+    settingsData = new Settings();
+    settingsData.setData();
+    settingsData.applySettings();
 }
 
 $(document).ready(function () {
 
-    $("footer").append('<div class="app-version">Plugin Pony Town UI: <b>1.1.0</b><div class="text-nowrap d-inline d-sm-block">by <a target="_blank" href="https://www.youtube.com/channel/UC-X7_-qML3aXqrDQ2UKLFkA">Mariana Ponyriama</a></div></div>');
+    let footerAppend = '<div class="app-version">Plugin Pony Town UI: <b>' + versionPlugin +'</b><div class="text-nowrap d-inline d-sm-block">by <a target="_blank" href="'+ linkToAuthor +'">Mariana Ponyriama</a></div></div>';
+    $("footer").append(footerAppend);
+    
 
     $(document).on("DOMNodeInserted", function (e) {
+        if($(e.target).is("footer"))
+        {
+            $(e.target).append(footerAppend);
+        }
+        if($(e.target).hasClass("modal"))
+        {
+            $("settings-modal .nav").append("<button id='MPPL-setting-plugin' class='btn-unstyled nav-link'>Plugin</button>")
+
+            $("#MPPL-setting-plugin").click(function()
+            {
+                $("body").append('<div class="MPPL-container"><div class="MPPL-settings"><div class="MPPL-modal-header">Settings for Plugin</div><div class="MPPL-modal-content"></div><div class="MPPL-modal-footer"><div class="MPPL-left"><div class="MPPL-version-setting">Version '+ versionPlugin +' by <a href="'+ linkToAuthor +'" target="_blank">Mariana Ponyriama</a></div></div><div class="MPPL-right"><button type="button" class="btn" id="MPPL-reset-plugin">Reset</button><button type="button" class="btn" id="MPPL-close-plugin">Close</button></div></div></div></div>');
+
+                $("#MPPL-close-plugin").click(function()
+                {
+                    $(".MPPL-container").remove();
+                });
+                $("#MPPL-reset-plugin").click(function()
+                {
+                    alert("Settings was set as default");
+                    standartSetting();
+                    $(".MPPL-container").remove();
+                });
+
+                // Тёмная тема
+                itemSettingsCheckbox("darkTheme", "Dark mode", function()
+                {
+                    settingsData.darkTheme = true;
+                    settingsData.setData();
+                    settingsData.applySettings();
+                }, function(){
+                    settingsData.darkTheme = false;
+                    settingsData.setData();
+                    settingsData.applySettings();
+                }, settingsData.darkTheme, "Turns on dark mode in the game interface");
+
+                // Закруглённость
+                itemSettingsCheckbox("squarePortrait", "Square portrait", function()
+                {
+                    settingsData.squareImage = true;
+                    settingsData.setData();
+                    settingsData.applySettings();
+                }, function(){
+                    settingsData.squareImage = false;
+                    settingsData.setData();
+                    settingsData.applySettings();
+                }, settingsData.squareImage, "Rounded square character's icons");
+
+                function itemSettingsCheckbox(id, label, func, unfunc, settedValue, sublabel = "")
+                {
+                    $(".MPPL-modal-content").append('<div class="item" id="'+ id + '-item' +'"><div class="top"><input type="checkbox" name="" id="'+ id +'"><label for="'+ id +'">'+ label +'</label></div></div>');
+                    if(sublabel != "")
+                    {
+                        $(document.getElementById(id + "-item")).append('<div class="subtop"><label for="'+ id +'">'+ sublabel +'</label></div>');
+                    }
+                    if(settedValue)
+                    {
+                        $(document.getElementById(id)).attr('checked', true);
+                    }
+                    else
+                    {
+                        $(document.getElementById(id)).attr('checked', false);
+                    }
+                    $(document.getElementById(id)).click(function()
+                    {
+                        if (document.getElementById(id).checked) {
+                              func();
+                          } else {
+                              unfunc();
+                          }
+                    })
+                }
+            });
+        }
+
         if ($(e.target).hasClass("friends-dropdown-menu")) {
             friends = new Array();
             searchFavourite = false;
@@ -149,8 +243,6 @@ $(document).ready(function () {
             });
         }
 
-
-
         if ($(e.target).hasClass("friends-item")) {
             let friend = new Friend(e.target);
             friends.push(friend);
@@ -198,6 +290,7 @@ $(document).ready(function () {
             });
         }
 
+        
 
     });
 
